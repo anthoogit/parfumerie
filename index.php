07acc8f16097c4320e47fcb193be7860efc63341
anthoogit <74049742+anthoogit@users.noreply.github.com>
2021-01-11 10:51:43 +0100

    profil client
    profil_client

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 <body>
 	<h1>Accueil</h1>
     <a href="produits.php">Produits</a><br/>
+	<a href="profil_client">Profil client</a><br/>
     <a href="clients.php">Clients</a><br/>
     <a href="pagecadeau.php">Cadeaux</a><br/>
     <a href="2ez4me.php">Coin VIP (only for 2ez4me members)</a>
