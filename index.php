ecf27986145702aa842753b922c96259cc37355a
Origami0067 <72131002+Origami0067@users.noreply.github.com>
2021-01-11 10:45:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	<h1>Accueil</h1>
     <a href="produits.php">Produits</a><br/>
     <a href="clients.php">Clients</a><br/>
+    <a href="pagecadeau.php">Cadeaux</a><br/>
     <a href="2ez4me.php">Coin VIP (only for 2ez4me members)</a>
 </body>
 </html>
