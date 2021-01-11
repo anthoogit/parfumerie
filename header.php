894f05806ac15e59715b6e852b843a843e23305f
anthoogit <74049742+anthoogit@users.noreply.github.com>
2021-01-11 12:01:47 +0100

    update

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <header>
-    <img src="logo.png">
+    <a href="index.php"><img src="logo.png"></a>
 	<div class="topnav">
 	
-	  <a class="active" href="#Accueil">Accueil</a>
-	  <a href="#news">Boutique</a>
-	  <a href="#contact">Les cadeaux</a>
-	  <a href="#about">Votre Compte</a>
+	    <a class="active" href="#Accueil">Accueil</a>
+	    <a href="#news">Boutique</a>
+	    <a href="#contact">Les cadeaux</a>
+	    <a href="#about">Votre Compte</a>
 	</div>
 </header>
--- profil_client.php
+++ profil_client.php
@@ -12,43 +12,44 @@ require('model.php');
 
 <body>
 <?php require('header.php'); ?>
-
-<h3>Produits</h3>
-<form action="" method="GET">
-	<label for="client_id">ID Client</label>
-	<input type="text" name="client_id" id="client_id" />
-	<input type="submit" value="Valider" />
-</form>
-
-<?php
-if (isset($_GET["client_id"])){
-	$client_id = $_GET["client_id"];
-	if (verifClient_ID($client_id)){
-		$client = getclient($client_id);
-	?>
-
-	<h3>Mes Informations</h3>
-	<p>
-		<?= "Code client : " . $client["CodeClient"]; ?><br />
-		<?= "Nom : " . $client["nom"]; ?><br />
-		<?= "Prénom : " . $client["prenom"]; ?><br />
-		<?= "Adresse : " . $client["adresse"]; ?><br />
-		<?= "facebook : " . $client["facebook"]; ?><br />
-		<?= "instagram : " . $client["instagram"]; ?><br />
-		<?= "email : " . $client["email"]; ?><br />
-		<?= "numeroTel : " . $client["numeroTel"]; ?><br />
-		<?= "nbPoints : " . $client["nbPoints"]; ?><br />
-		<?= "id_membership : " . $client["id_membership"]; ?><br />
-		<?= "dateExpiration : " . $client["dateExpiration"]; ?><br />
-	</p>
+<div id="bloc_page">
+	<h3>Mon profil</h3>
+	<form action="" method="GET">
+		<label for="client_id">ID Client</label>
+		<input type="text" name="client_id" id="client_id" />
+		<input type="submit" value="Valider" />
+	</form>
 
 	<?php
-	}
-	else{
+	if (isset($_GET["client_id"])){
+		$client_id = $_GET["client_id"];
+		if (verifClient_ID($client_id)){
+			$client = getclient($client_id);
+		?>
 
+		<h3>Mes Informations</h3>
+		<p>
+			<?= "Code client : " . $client["CodeClient"]; ?><br />
+			<?= "Nom : " . $client["nom"]; ?><br />
+			<?= "Prénom : " . $client["prenom"]; ?><br />
+			<?= "Adresse : " . $client["adresse"]; ?><br />
+			<?= "facebook : " . $client["facebook"]; ?><br />
+			<?= "instagram : " . $client["instagram"]; ?><br />
+			<?= "email : " . $client["email"]; ?><br />
+			<?= "numeroTel : " . $client["numeroTel"]; ?><br />
+			<?= "nbPoints : " . $client["nbPoints"]; ?><br />
+			<?= "id_membership : " . $client["id_membership"]; ?><br />
+			<?= "dateExpiration : " . $client["dateExpiration"]; ?><br />
+		</p>
+
+		<?php
+		}
+		else{
+
+		}
 	}
-}
-?>
+	?>
+</div>
 
 
 </body>
