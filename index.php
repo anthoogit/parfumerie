3f754d17a729fba28cb176de901745aad7a4b175
Moon-Lens <62239689+Moon-Lens@users.noreply.github.com>
2021-01-11 10:46:31 +0100

    qsd
    qsd

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
+
+
+
 <head>
+
+	<div class="topnav">
+	  <a class="active" href="#Accueil">Accueil</a>
+	  <a href="#news">Boutique</a>
+	  <a href="#contact">Les cadeaux</a>
+	  <a href="#about">Votre Compte</a>
+	</div>
+
+
 	<meta charset="utf-8">
 	<title>Accueil</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	
 </head>
 
 <body>
