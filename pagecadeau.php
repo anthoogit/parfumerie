472733268fa9e04bd65247975b21f6b295be20ec
Origami0067 <72131002+Origami0067@users.noreply.github.com>
2021-01-11 10:31:33 +0100

    Add files via upload

---
+++ pagecadeau.php
@@ -0,0 +1,37 @@
+<?php
+require('model/model.php');
+$req = getProduits();
+?>
+
+<h3>Cadeaux</h3>
+<p>
+	<table>
+		<thead>
+			<tr>
+				<th>noCadeau</th>
+				<th>nomCadeau</th>
+				<th>prixFidelite</th>
+				<th>niveauMembership</th>
+				<th>stock</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+			while ($cadeau = $req->fetch()) {
+			?>
+
+				<tr>
+					<td><?= $cadeau['noCadeau'] ?></td>
+					<td><?= $cadeau['nomCadeau'] ?></td>
+					<td><?= $cadeau['prixFidelite'] ?></td>
+					<td><?= $cadeau['niveauMembership'] ?></td>
+					<td><?= $cadeau['stock'] ?></td>
+				</tr>
+			<?php
+			}
+			?>
+		</tbody>
+	</table>
+</p>
+</div>
