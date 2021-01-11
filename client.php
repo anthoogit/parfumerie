4b5fa923a9ef1f51f31e8d6053033088e3ea9496
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-01-11 10:19:28 +0100

    Create client.php

---
+++ client.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Clients</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<h1>Liste des clients</h1>
+    <a href="produits.php">Produits</a>
+</body>
+</html>
