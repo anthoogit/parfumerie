93cb17a5d494f18477fbad120c7298c4d2f33ab8
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-01-11 10:39:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,7 @@
 <body>
 	<h1>Accueil</h1>
     <a href="produits.php">Produits</a>
+    <a href="clients.php">Clients</a>
+    <a href="2ez4me.php">Coin VIP (only for 2ez4me members)</a>
 </body>
 </html>
