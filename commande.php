096935172c495bf60c42938623069072870053be
anthoogit <74049742+anthoogit@users.noreply.github.com>
2021-01-13 17:27:10 +0100

    Update commande.php

--- commande.php
+++ commande.php
@@ -14,7 +14,7 @@ require('model.php');
 <?php require('header.php'); ?>
 
 <div id="bloc_page">
-	<h1 class="titre_principal"><span>Mes commandes</span></h1>
+	<h1 class="titre_principal"><span>Infos commande</span></h1>
 	<form action="" method="GET">
 		<label for="commande_id">ID Commande</label>
 		<input type="text" name="commande_id" id="commande_id" />
