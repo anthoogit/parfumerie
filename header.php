a5aa2eff4c6ba52068ed88cbae647ceb5728392e
Moon-Lens <62239689+Moon-Lens@users.noreply.github.com>
2021-01-13 15:09:01 +0100

    qsd
    qsd

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <header>
-	
     <a href="index.php"><img src="logo.png"></a>
 	<div class="topnav">
 	
-	    <a class="active" href="#Accueil">Accueil</a>
-	    <a href="#news">Boutique</a>
-	    <a href="#contact">Les cadeaux</a>
-	    <a href="#about">Votre Compte</a>
+	    <a class="active" href="index.php">Accueil</a>
+	    <a href="produits.php">Boutique</a>
+	    <a href="pagecadeau.php">Les cadeaux</a>
+	    <a href="profil_client.php?client_id=1">Mon Compte</a>
 	</div>
 </header>
