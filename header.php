ef97d776250f238742e349902bccc3857356f210
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-02-05 14:05:11 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,31 @@
+<?php 
+session_start();
+require('model.php'); 
+?>
 <header>
     <a href="index.php"><img src="logo.png"></a>
 	<div class="topnav">
 	
 	    <a class="active" href="index.php">Accueil</a>
-	    <a href="#news">Boutique</a>
-	    <a href="#contact">Les cadeaux</a>
-	    <a href="#about">Votre Compte</a>
+	    <a href="#">Boutique</a>
+	    <a href="pagecadeau.php">Les cadeaux</a>
+		<?php
+		if(isset($_SESSION['loggedin'])){
+			if($_SESSION['loggedin'] && $_SESSION['loggedin'] == true){
+				$user = $_SESSION['username'];
+				
+				//récupérer l'id du client
+				$id_client = getClientFromLogin($_SESSION['username']);
+				
+				// afficher un message
+				echo '<a href="profil_client.php?client_id='.$id_client['id_client'].'">Mon compte ('.$user.')</a>';
+				echo '<a href="logout.php">Se déconnecter</a>';
+			}
+		} else {
+			echo '<a href="login.php">Se connecter</a>';
+		}
+		?>
+		
+		
 	</div>
 </header>
