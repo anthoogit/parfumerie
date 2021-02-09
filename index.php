85b134210329748dca8248012009aa442067f4e5
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-02-09 18:56:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,17 +27,25 @@
 			// afficher un message
 			echo "<h2>Vous avez ".$client['nbPoints']." points !</h2><br/>";
 			echo '<div class="bouton_catalogue"><a href="pagecadeau.php">Voir le catalogue de cadeaux</a></div><br/><br/>';
+		
+			if($_SESSION['username'] == "admin"){
+				echo '<div class="bouton_catalogue"><a href="produits.php">Produits [ADMIN ONLY]</a></div><br/><br/>';
+				echo '<div class="bouton_catalogue"><a href="clients.php">Clients [ADMIN ONLY]</a></div><br/><br/>';
+				echo '<div class="bouton_catalogue"><a href="commandes.php">Commandes [ADMIN ONLY]</a></div><br/><br/>';
+				echo '<div class="bouton_catalogue"><a href="profil_client.php?client_id=1">Profil client (A DEPLACER DANS CLIENTS.PHP)</a></div><br/><br/>';
+				echo '<div class="bouton_catalogue"><a href="info-commande.php?commande_id=1">Info commande</a></div><br/>';
+			}
+		
 		}
 	} else {
 		echo "Connectez-vous pour accéder au large choix de cadeaux !<br/><br/>";
 	}
+
+
+
 	?>
 
-	<a href="produits.php">Produits (à afficher que pour l'admin)</a><br/>
-	<a href="clients.php">Clients (à afficher que pour l'admin)</a><br/>
-	<a href="commandes.php">Commandes (à afficher que pour l'admin)</a><br/>
-	<a href="profil_client.php?client_id=1">Profil client (A DEPLACER DANS CLIENTS.PHP)</a><br/>
-	<a href="info-commande.php?commande_id=1">Info commande</a><br/>
+	
 </div>
 
 <?php require('footer.php'); ?>
